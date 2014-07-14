10e6e463862c9f83204e6889617fdabb9a669f52
seethakris <seethakris@gmail.com>
2014-07-14 15:40:00 +0800

    Ask user for template sitmulus

--- batch_process.m
+++ batch_process.m
@@ -5,23 +5,36 @@ function batch_process
 % User Input
 Folder_Name = fullfile('/Volumes/Se/Michelle_Data/Data/-140617 Fish4 KR15-3A Deconvolved/'); %Folder with data
 x = 50; %Number of pixels to include in square ROI [x y] and get mean activity within those pixels
-y = 50; 
+y = 50;
 Baseline_strt = 1; %Start time of baseline
 Baseline_end = 10; %End time of baseline
 
 
-%% Step1
-% Convert .stk to .tif
-[Stimulus_List, T,Z] = read_images(Folder_Name);
-save([Folder_Name, 'Experiment_Parameters.mat'], 'T', 'Z', 'Stimulus_List')
+% %% Step1
+% % Convert .stk to .tif
+% [Stimulus_List, T,Z] = read_images(Folder_Name);
+% save([Folder_Name, 'Experiment_Parameters.mat'], 'T', 'Z', 'Stimulus_List')
+
 
 %% Step 2
-% Go around image using a square ROI, get OB pixels and mean intensity of those
-% pixels within ROI
+% Ask user Which stimulus to use as template?
 load([Folder_Name, 'Experiment_Parameters.mat'])
-makeROIS(Folder_Name,Stimulus_List, T, Z, x,y, Baseline_strt, Baseline_end)
+[selection,ok_or_not] = listdlg('PromptString','Select an odor stimulus to use as template:',...
+    'SelectionMode','single',...
+    'ListString',Stimulus_List);
+
+if ok_or_not == 1
+    Use_Stim = selection;
+else
+    return;
+end
 
 %% Step 3
+% Go around image using a square ROI, get OB pixels and mean intensity of those
+% pixels within ROI
+makeROIS(Folder_Name,Stimulus_List, T, Z, x,y, Baseline_strt, Baseline_end, Use_Stim)
+
+%% Step 4
 % Do PCA and plot
 % Temporal PCA with cells as eigenvector
 load([Folder_Name, 'Experiment_Parameters.mat'])
